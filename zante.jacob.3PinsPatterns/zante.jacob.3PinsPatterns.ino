8a0e114bb525f5e7af6360d75a2482b3e1a108ac
destroyer183 <jzante99@gmail.com>
2024-11-08 10:49:50 -0500

    create repo

---
+++ zante.jacob.3PinsPatterns/zante.jacob.3PinsPatterns.ino
@@ -0,0 +1,19 @@
+/*
+Student Name: Jacob Zante
+Teacher Name: Hayden So
+Title of Project: 
+Brief Description of Project: 
+Date of Completion: 
+*/
+
+
+
+void setup() {
+
+}
+
+
+
+void loop() {
+
+}
